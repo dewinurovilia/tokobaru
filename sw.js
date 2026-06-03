6d19c4da0c4c4a18cf10c8f5b30ed39bc3be13d9
dewinurovilia <fauzy2327@gmail.com>
2026-06-03 14:54:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,59 @@
-const CACHE_NAME = 'toko-defana-v2';
+const CACHE_NAME = "toko-defana-v14";
 
 const urlsToCache = [
-
-'./',
-'./index.html',
-'./style.css',
-'./script.js'
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./img/barcode.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-
-.then(cache => {
-
-return cache.addAll(urlsToCache);
-
-})
-
-);
-
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
 
-event.respondWith(
+        return null;
+      })
+    ))
+  );
 
-caches.match(event.request)
+  self.clients.claim();
+});
 
-.then(response => {
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
 
-return response || fetch(event.request);
+  if (event.request.method !== "GET" || url.origin !== self.location.origin) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
 
-})
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
 
-);
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone).catch(() => {});
+        });
 
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
